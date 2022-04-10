4e8b38077d114a4cfc207ff0b69d6861ceab211f
Jose Brandao <josepedrosbr@gmail.com>
2022-04-10 11:56:00 -0300

    Add makemigrations files and register models on admin Namespaced the app

--- bigdoc/admin.py
+++ bigdoc/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
+from .models import *
+
 # Register your models here.
+
+
+admin.site.register(Doenca)
+admin.site.register(Sintoma)
+admin.site.register(Diagnostico)
