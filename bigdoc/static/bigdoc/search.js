cddb6b596ea9d85f72dc7c77c352f5c887ca677e
Jose Brandao <josepedrosbr@gmail.com>
2022-04-18 01:18:52 -0300

    Search: Add info text for when there is no results to show Fixed unintended form submission by hitting enter for when there is ongoing search

--- bigdoc/static/bigdoc/search.js
+++ bigdoc/static/bigdoc/search.js
@@ -1,22 +1,34 @@
-var sintomas = document.getElementsByName('sintomas');
+const sintomas = document.getElementsByName('sintomas');
 
-var searchbox = document.getElementById('search')
-var btnDiagnosticar = document.getElementById('diagnosticar')
+const searchbox = document.getElementById('search');
+const btnDiagnosticar = document.getElementById('diagnosticar');
 
-searchbox.addEventListener('input', function(event) {
-    console.log(this.value);
+const p = document.getElementById('not-found');
+
+searchbox.addEventListener('input', function() {
+    p.classList.add("visually-hidden");
+
     if (!this.value) {
+        btnDiagnosticar.disabled = false;
         btnDiagnosticar.classList.remove("visually-hidden");
 
         sintomas.forEach(sintoma => {
             sintoma.closest("div").classList.remove("visually-hidden");
         })
     } else {
+        btnDiagnosticar.disabled = true;
         btnDiagnosticar.classList.add("visually-hidden");
 
+        var counter = 0;
         sintomas.forEach(sintoma => {
             if (!sintoma.value.toLowerCase().includes(this.value.toLowerCase())) {
                 sintoma.closest("div").classList.add("visually-hidden");
+                
+                counter += 1;
+                
+                if (counter == sintomas.length) {
+                    p.classList.remove("visually-hidden");
+                }
             } else {
                 sintoma.closest("div").classList.remove("visually-hidden");
             }
